e79db1966d403d6b5866f95c0da658abb801e56e
Singh, Arbind <Arbind.Singh@LibertyMutual.com>
2018-06-01 11:45:37 -0400

    LRU Cache

---
+++ src/IncreasingSequences.java
@@ -0,0 +1,17 @@
+
+public class IncreasingSequences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr1 = {5, 3, 7, 7, 10};
+		int[] arr2 = {1, 6, 6, 9, 9};
+		
+		int steps = solution(arr1, arr2, arr1.length);
+
+	}
+	
+	static int solution(int A[], int B[], int N){
+		return 0;
+	}
+
+}
---
+++ src/LRUCache.java
@@ -0,0 +1,102 @@
+import java.util.HashMap;
+
+class Node{
+	int key;
+	String value;
+	Node next;
+	Node prev;
+	public Node (int key,String value){
+		this.key= key;
+		this.value=value;
+	}
+}
+public class LRUCache {
+	Node head = null;
+	Node end = null;
+	HashMap<Integer, Node> map = new HashMap<Integer , Node>();
+
+	public String get(int key){
+		if (map.containsKey(key)){
+			Node n = map.get(key) ;
+			remove (n);
+			setHead(n);
+			return n.value;
+		}
+		return null;
+	}
+	
+	public void remove(Node n){
+		if (n.prev != null)
+			n.prev.next = n.next;
+		else
+			head = n.next;
+		
+		if (n.next != null)
+			n.next.prev = n.prev;
+		else
+			end = n.prev;
+
+	}
+
+	public void setHead(Node n){
+		n.next = head;
+		n.prev = null;
+		
+		if (head != null)
+			head.prev = n;
+		head = n;
+		
+		if (end == null)
+			end = head;
+	}
+
+	public void set(int key, String value){
+		if (map.containsKey(key)){
+			Node n = map.get(key);
+			n.value=value;
+			remove(n);
+			setHead(n);
+		}
+		else {
+			Node newNode = new Node (key,value);
+			setHead(newNode);
+			map.put(key, newNode);
+		}
+	}
+	
+	public void printCache(){
+		Node temp = new Node(0,null);
+		temp = head;
+		System.out.print("Cache Element : ");
+		while (temp != null){
+			System.out.print(temp.value+ " ");
+			temp = temp.next;
+		}
+		System.out.println("");
+	}
+	
+	public static void main(String[] args)
+
+	{
+
+	LRUCache lr=new LRUCache();
+	lr.set(1, "1");
+	lr.set(2,"2");
+	lr.set(3, "3");
+	lr.set(4, "4");
+	lr.set(5, "5");
+	lr.printCache();
+	String val=lr.get(1);
+	System.out.println("Get 1 ");
+	lr.printCache();
+	lr.set(6, "6");
+	System.out.println("Insert 6 ");
+	lr.printCache();
+	String val2=lr.get(2);
+	System.out.println("get 2 ");
+	lr.printCache();
+
+	}
+	
+	
+}
